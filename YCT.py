3cf11bdb2f71ffdf78264f862d583d21e2dfad16
Manuel Castaneda <Ma52Castaneda@gmail.com>
2019-08-05 18:50:43 -0700

    added a ton of debug prints aswell as some lines that determine how bright the lights will be. ALSO I stoped using time.sleep because i still need to always be listening for input. I made my own little timer.

--- YCT.py
+++ YCT.py
@@ -1,7 +1,13 @@
 import urllib.request
 import json
+import time
 
-key = "AIzaSyD9xf4yeziXrBDuNgSimq9XarHmVvmHDgs"
+scanFrequency = 10 #these are in seconds
+loopBrakes = 0
+
+
+
+key           = "AIzaSyD9xf4yeziXrBDuNgSimq9XarHmVvmHDgs"
 #Hard coded url: https://www.googleapis.com/youtube/v3/channels?part=statistics&id=UCT5_uqXPSVUaL5r2ChcBVeg&key=AIzaSyD9xf4yeziXrBDuNgSimq9XarHmVvmHDgs
 
 class channel:
@@ -9,44 +15,63 @@ class channel:
         self.name   = name
         self.id     = id
         self.videos = videoCount
-        self.subs   = self.subCount(0,0)
-        self.views  = self.viewCount(0,0)
+        self.subs   = self.subCount(0, 0, 0)
+        self.views  = self.viewCount(0, 0, 0, 0)
 
     class subCount:
-        def __init__(self, current, original):
+        def __init__(self, current, original, goal):
             self.current  = current
             self.original = original
             self.increase = current - original
+            self.goal     = goal
 
     class viewCount:
-        def __init__(self, current, original):
+        def __init__(self, current, original, average, goal):
             self.current  = current
             self.original = original 
             self.increase = current - original
+            self.average  = average
+            self.goal     = goal
             
 
 adrianas = channel("Adrianita's channel", "UCT5_uqXPSVUaL5r2ChcBVeg", 1)
+adrianas.subs.goal = 10
+adrianas.views.goal = 50
+    #the goal controls how bright the light will be. If you meet/exceed the goal then the light will be lit at the max
 
 
 def getThemSubs():
     data = urllib.request.urlopen("https://www.googleapis.com/youtube/v3/channels?part=statistics&id=" + adrianas.id +"&key=" + key).read()
     adrianas.subs.current = json.loads(data)["items"][0]["statistics"]["subscriberCount"]
 
+
+
+    
+
 def getThemViews():
     data = urllib.request.urlopen("https://www.googleapis.com/youtube/v3/channels?part=statistics&id=" + adrianas.id +"&key=" + key).read()
     adrianas.views.current = json.loads(data)["items"][0]["statistics"]["viewCount"]
-    adrianas.videos = json.loads(data)["items"][0]["statistics"]["videoCount"]
- 
-getThemSubs()
-getThemViews()
+    adrianas.views.current = int(adrianas.views.current)
 
-print("AA " + str(adrianas.videos))
-print("AA " + float(adrianas.views.current/adrianas.videos))
+    adrianas.videos = json.loads(data)["items"][0]["statistics"]["videoCount"]
+    adrianas.videos = int(adrianas.videos)
+    adrianas.views.average = adrianas.views.current/adrianas.videos * 1.0 
+
+
+
+def Scan():
+    getThemSubs()
+    getThemViews()
+
+Scan()
+
+
+
 
 #Initial print
 print("Adrianita has " + "{:,d}".format(int(adrianas.subs.current)) + " subscribers!")
 print("Adrianita has " + "{:,d}".format(int(adrianas.views.current)) + " total views!")
-print("Adrianita has an average of " + "{:,d}".format(int(adrianas.views.average)) + " views per video")
+print("Adrianita has an average of " + str(adrianas.views.average) + " views per video")
 
 
 # give the original counts
@@ -54,22 +79,47 @@ adrianas.subs.original  = adrianas.subs.current
 adrianas.views.original = adrianas.views.current
 
 
-while(1==1):
-    
-    if(adrianas.subs.increase > 1):
-        print("NEW SUBSCRIBER!")
-        adrianas.subs.original = adrianas.subs.current
-    
-    if(adrianas.views.increase > 1):
-        print("NEW VIEW!")
-        adrianas.views.original = adrianas.views.current
+while (1==1):
+    if(time.time() > loopBrakes):
+        
+        print("-----")
+        Scan()
+
+        print("current subs: " + str(adrianas.subs.current))
+        print("original subs: " + str(adrianas.subs.original))
+
+        adrianas.subs.increase = int(adrianas.subs.current) - int(adrianas.subs.original)
+        adrianas.views.increase = int(adrianas.views.current) - int(adrianas.views.original)
+
+        print("increase in subs: " + str(adrianas.subs.increase))
 
 
-#make a function that calculates the amount of actual subscribers/views minues the ogSubs/ogViews 
-# the answer to this could determine how bright the LED light will be
-# Then you push a button and the lights go back to 0 brightness
-#this way, you could wake up or go to work/school and when you come back you...
-#... can see how many views/subs you got while you were gone. The button will reset the brightness
+        if(adrianas.subs.increase > 0):
+            print("NEW SUBSCRIBER!")
+            if(adrianas.subs.increase < adrianas.subs.goal):
+                subsBrightness = (adrianas.subs.increase/adrianas.subs.goal) * 100
+                print("Subs light at " + str(subsBrightness) + " brightness" )
+            
+    
+        if(adrianas.views.increase > 0):
+            print("NEW VIEW!")
+            if(adrianas.views.increase < adrianas.views.goal):
+                viewsBrightness = (adrianas.views.increase/adrianas.views.goal) * 100
+                print("Views light at " + str(viewsBrightness) + " brightness" )
+            
+        
+
+        if(1 == 2): #change to if the button is pressed
+            adrianas.subs.original = adrianas.subs.current
+            adrianas.views.original = adrianas.views.current
+
+        loopBrakes = time.time() + scanFrequency #makes the loop happen x seconds later
+
+
+
+
+
+
 
     
     
