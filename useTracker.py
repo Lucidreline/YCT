e63f9e8333b4ef8d24374f8078dee1fc8d95018a
Manuel Castaneda <Ma52Castaneda@gmail.com>
2019-08-08 15:49:31 -0700

    made a typo

--- useTracker.py
+++ useTracker.py
@@ -1,6 +1,6 @@
 import YCT
 
-adrianas = YTC.channel("Adrianita", "UCT5_uqXPSVUaL5r2ChcBVeg", 5, 25)
+adrianas = YCT.channel("Adrianita", "UCT5_uqXPSVUaL5r2ChcBVeg", 5, 25)
 #this created the channel and initialized a few things
 
-YTC.AnalyzeChannel(adrianas)
+YCT.AnalyzeChannel(adrianas)
